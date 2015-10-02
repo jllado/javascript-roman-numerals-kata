a0c4aee83aec4aa979593202acd36af5536c124f
Juan Llado <jllado@gmail.com>
2015-10-02 13:10:07 +0200

    Refactor: remove duplication from roman numbers.

--- src/roman-numerals.js
+++ src/roman-numerals.js
@@ -3,11 +3,11 @@ var roman_numbers = {
   1: "I",
   4: "IV",
   5: "V",
-  6: "VI",
-  7: "VII"
 }
 function toRoman(number) {
   if (roman_numbers[number])
     return roman_numbers[number];
+  if (number > 5)
+    return "V" + toRoman(number - 5);
   return "I" + toRoman(number - 1);
 }
