388ce3b57f8bb6634b2a3feaa12c4cf180febdff
Juan Llado <jllado@gmail.com>
2015-10-02 13:13:10 +0200

    Refactor: remove duplication from test.

--- test/roman-numerals.spec.js
+++ test/roman-numerals.spec.js
@@ -10,26 +10,14 @@
  * IX -> 9
  * X -> 10
  */
-describe('roman numerals', function () {
-  it("should return I when 1", function () {
+describe('roman numerals converter', function () {
+  it("should convert integer to roman number", function () {
     expect(toRoman(1)).toEqual("I");
-  });
-  it("should return II when 2", function () {
     expect(toRoman(2)).toEqual("II");
-  });
-  it("should return III when 3", function () {
     expect(toRoman(3)).toEqual("III");
-  });
-  it("should return IV when 4", function () {
     expect(toRoman(4)).toEqual("IV");
-  });
-  it("should return V when 5", function () {
     expect(toRoman(5)).toEqual("V");
-  });
-  it("should return VI when 6", function () {
     expect(toRoman(6)).toEqual("VI");
-  });
-  it("should return VII when 7", function () {
     expect(toRoman(7)).toEqual("VII");
   });
 });
